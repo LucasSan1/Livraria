a6505620ba92ee739d1765ec035dac952097e9eb
LucasSan1 <137550683+LucasSan1@users.noreply.github.com>
2023-09-11 18:25:52 -0300

    Add files via upload

---
+++ src/componentes/header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export default function Header(){
+    return(
+        <header className="header">
+            <h2>Livraria</h2>
+        </header>
+    );
+}
