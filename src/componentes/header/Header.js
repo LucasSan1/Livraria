5eaabb6f4127e9fbdb99b9920025b4649433267f
LucasSan1 <137550683+LucasSan1@users.noreply.github.com>
2023-09-13 17:12:00 -0300

    Add files via upload

---
+++ src/componentes/header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+export default function Header(){
+    return(
+        <a href="/">
+            <header className="header">
+                <h2>Livraria</h2>
+            </header>
+        </a>
+    );
+}
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './style.css';
+import Card from '../../componentes/card/Card';
+
+export default function Home(){
+    return(
+        <Card livro = "OL22449759M"></Card>
+    )
+}
