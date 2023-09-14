baaca0642e79b833c8f32870354fca5b8f35279b
LucasSan1 <137550683+LucasSan1@users.noreply.github.com>
2023-09-14 17:15:38 -0300

    Delete src directory

--- src/pages/Home/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './style.css';
-import Card from '../../componentes/card/Card';
-
-export default function Home(){
-    return(
-        <Card livro = "OL22449759M"></Card>
-    )
-}
